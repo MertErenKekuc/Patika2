8e0f2f56a0fea26a25ddf18774904ea1a9fd02c5
Mert Eren Keküç <merterenkekuc@gmail.com>
2022-11-25 17:02:03 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Patika2
+{
+    public class opertorler
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 6;
+
+            a += 5;
+            b *= 1;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+            bool tr = true;
+            bool fl = false;
+
+            if(tr&&fl)
+                Console.WriteLine("xxxxxxx");
+            else if(tr&&!fl)
+                Console.WriteLine("aaaaaa");
+
+            bool ab = a > b;
+            bool abc = a < b;
+
+            Console.WriteLine(ab); 
+            Console.WriteLine(abc);
+
+            a++;
+           int sonuc = a % 3;
+            Console.WriteLine(sonuc);
+        }
+    }
+}
